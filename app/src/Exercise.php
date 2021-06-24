1112663166b392ffaecfc25f54510110c6aacb7a
Laszlo Heredy <laszlo.heredy@icloud.com>
2021-06-23 23:37:27 -0700

    running index built

--- app/src/Exercise.php
+++ app/src/Exercise.php
@@ -4,9 +4,12 @@ class Exercise
 {
     protected $drivers   = [];
     protected $addresses = [];
-    protected $index     = [];
 
-    protected $output;
+    /**
+     * @var array keyed by both "$driver $address" and "$address $driver", each with value suitability score for the pair.
+     */
+    protected $index  = [];
+    protected $output = [];
 
     public function __construct()
     {
@@ -18,7 +21,25 @@ class Exercise
 
     public function doTheExercise(): void
     {
+        $this->doFillIndexes();
 
+        $this->output = $this->index;
+    }
+
+    /**
+     * Populate index with Suitability Score for the driver, address pair, both ways.
+     *
+     * @throws Exception
+     */
+    public function doFillIndexes(): void
+    {
+        foreach ($this->drivers as $driver) {
+            foreach ($this->addresses as $address) {
+                $suitability_score = Util::getSuitabilityScore($address, $driver);
+
+                $this->index["$driver $address"] = $suitability_score;
+            }
+        }
     }
 
     public function __toString(): string
